97748d07f6039cd441daba63b0cb42f55b398fd6
Santhosh Rajendran <139849562+Santhosh-1703@users.noreply.github.com>
2024-06-26 10:43:56 +0530

    Update settings.py

--- portfolioSR/settings.py
+++ portfolioSR/settings.py
@@ -17,12 +17,6 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
-
-env = environ.Env()
-
-environ.Env.read_env()
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
